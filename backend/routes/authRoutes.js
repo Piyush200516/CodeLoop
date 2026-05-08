2b3fcbf2c922e73d89a9ba37848336157afc31b0
Piyush Mishra <piyushmishra21052003@gmail.com>
2026-05-08 23:10:17 +0530

    update-authRoutes.js

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const router = express.Router();
+const {registerUser, loginUser, logoutUser, getMe, verifyOTP, resendOTP, googleLogin} = reequire('../controllers/authcontroller');
+
 
 const {
   registerUser,
